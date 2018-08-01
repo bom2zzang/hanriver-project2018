72796ad92e4bff00ee863b25e30dd570621de3f7
tjr7788 <sig4213@naver.com>
2018-08-01 19:20:37 +0900

    .

---
+++ hanriver-project/src/main/webapp/html/member/list.js
@@ -0,0 +1,50 @@
+"use strict"
+
+var {page, size} = $.parseQuery(location.href);
+
+var tbody = $('#eListTable > tbody');
+let data = null;
+
+if (size != undefined && page != undefined)
+    loadList(page, size);
+else loadList(1, 3);
+
+$(ePrevBtn).click(function() {
+    loadList(data.page - 1, data.size);
+});
+
+$(eNextBtn).click(function() {
+    loadList(data.page + 1, data.size);
+});
+
+
+function loadList(page, size) {
+    $.getJSON(serverApiAddr + '/json/member/list', 
+        {
+            page: page,
+            size: size
+        }, function(result) {
+            data = result;
+            tbody.html('');
+            for (var item of data.list) {
+                $("<tr>").html(
+                    `<td>${item.no}</td><td><a href='#' data-id='${item.id}' class='viewLink'>${item.id}</a></td>
+                    <td>${item.email}</td><td>${item.tel}</td>`).appendTo(tbody);
+            }
+            $(ePageNo).html(data.page);
+            if (data.page <= 1)
+                $(ePrevBtn).attr('disabled', '');
+            else 
+                $(ePrevBtn).removeAttr('disabled');
+            
+            if (data.page >= data.totalPage)
+                $(eNextBtn).attr('disabled', '');
+            else
+                $(eNextBtn).removeAttr('disabled');
+        });
+}
+tbody.on('click', 'a.viewLink', function(event) {
+    event.preventDefault();
+    var id = $(event.target).attr('data-id');
+    location.href = `view.html?id=${id}&page=${data.page}&size=${data.size}`;
+});
